9a76b85c76f314729c94d63be1d38150e14be1e1
Triple_Nik <73399752+NikBoi@users.noreply.github.com>
2021-06-04 18:47:38 -0400

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,42 @@
+@echo off
+title Start
+echo Starting Process...
+timeout 5
+
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
+start "" https://www.cnn.com/politics
